8dd115ac54739bc895553ea8f7a69b85e3ad9810
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-20 20:16:55 +0300

    adding book entity

--- Student-Management-System/src/main/java/entity/Book.java
+++ Student-Management-System/src/main/java/entity/Book.java
@@ -1,4 +1,30 @@
 package entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private int bookId;
+
+    @Size(min = 6, message = "Title of book must have at at least 6 characters !!")
+    private String bookTitle;
+
+    @Size(min = 6, message = "Author name must have at least 6 characters !!")
+    private String bookAuthor;
 }
