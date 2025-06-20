2c9ff970e2cf391dbae35f505212fd88fd34e91d
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-20 20:17:28 +0300

    adding cours entity

--- Student-Management-System/src/main/java/entity/Course.java
+++ Student-Management-System/src/main/java/entity/Course.java
@@ -1,4 +1,29 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "courses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Course {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(min = 3, message = "name of course must have at least 3 characters !!")
+    private String name;
+
+    @Size(min = 10, message = "name of course must have at least 10 characters !!")
+    private String description;
 }
