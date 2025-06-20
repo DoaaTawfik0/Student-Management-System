a8bd8439f2d1c194f1b472b5e88e791a084322a9
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-20 21:19:49 +0300

    adding class for course service

---
+++ Student-Management-System/src/main/java/service/CourseService.java
@@ -0,0 +1,59 @@
+package service;
+
+import entity.Course;
+import exception.NotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import repository.CourseRepository;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    private CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course getCourseById(int id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+    public Course addCourse(@Valid Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course updateCourse(int id, @Valid Course updatedCourse) {
+        Course existingCourse = checkCourseExist(id);
+
+        existingCourse.setName(updatedCourse.getName());
+        existingCourse.setDescription(updatedCourse.getDescription());
+
+        courseRepository.save(existingCourse);
+
+        return existingCourse;
+    }
+
+
+    public void deleteCourse(int id) {
+        checkCourseExist(id);
+
+        courseRepository.deleteById(id);
+    }
+
+
+    private Course checkCourseExist(int id) {
+        Course existingCourse = getCourseById(id);
+        if (existingCourse == null)
+            throw new NotFoundException("Course not found with id: " + id);
+
+        return existingCourse;
+    }
+
+}
