fc9b0312a67a2123a06fd266ac33480cf1158071
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-20 21:17:15 +0300

    adding class for book service

--- Student-Management-System/src/main/java/service/BookService.java
+++ Student-Management-System/src/main/java/service/BookService.java
@@ -1,4 +1,67 @@
 package service;
 
+import entity.Book;
+import exception.NotFoundException;
+import org.springframework.stereotype.Service;
+import repository.BookRepository;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+
+    public Book getBookById(int id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+
+    public Book addBook(Book Book) {
+        return bookRepository.save(Book);
+    }
+
+    public Book updateBook(int id, Book updatedBook) {
+        Book existingBook = checkBookExist(id);
+
+        existingBook.setBookTitle(updatedBook.getBookTitle());
+        existingBook.setBookAuthor(updatedBook.getBookAuthor());
+        existingBook.setStudent(updatedBook.getStudent());
+
+        /* apply updates by saving new data */
+        bookRepository.save(existingBook);
+
+        return existingBook;
+    }
+
+    public void deleteBook(int id) {
+        checkBookExist(id);
+
+        bookRepository.deleteById(id);
+    }
+
+    public void deleteBookFromStudent(int studentId, int bookId) {
+        Book book = bookRepository.findByIdAndStudentId(bookId, studentId)
+                .orElseThrow(() -> new NotFoundException("Book not found or doesn't belong to student"));
+
+        book.setStudent(null); // Detach book from student
+        bookRepository.save(book); // Persist the change
+    }
+
+    private Book checkBookExist(int id) {
+        Book existingBook = getBookById(id);
+        if (existingBook == null)
+            throw new NotFoundException("Book not found with id: " + id);
+
+        return existingBook;
+    }
+
 }
