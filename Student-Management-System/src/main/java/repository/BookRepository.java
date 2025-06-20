71935336e00e840ffc08e043375db002a3271561
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-20 21:16:50 +0300

    adding method to get book that belongs to specific students

--- Student-Management-System/src/main/java/repository/BookRepository.java
+++ Student-Management-System/src/main/java/repository/BookRepository.java
@@ -3,5 +3,8 @@ package repository;
 import entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Optional<Book> findByIdAndStudentId(int bookId, int studentId);
 }
