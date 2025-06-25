de947006e9593146b5caa91692a72c6745ebd199
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-06-25 15:00:49 +0300

    Adding production and development environments and test using different profiles with different variables data

--- Student-Management-System/src/main/java/com/LearningSpringBoot/Student/Management/System/StudentManagementSystemApplication.java
+++ Student-Management-System/src/main/java/com/LearningSpringBoot/Student/Management/System/StudentManagementSystemApplication.java
@@ -1,13 +1,24 @@
 package com.LearningSpringBoot.Student.Management.System;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
+	@Value("${URL}")
+	private String URL;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
+
+	@PostConstruct
+	public void printEnvironmentVariable() {
+		System.out.println("Value of URL: " + URL);
+	}
+
 
 }
